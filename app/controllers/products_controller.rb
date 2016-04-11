de2a0073a565c7472a4b46aece948ac64dbf4f0e
Guin <guinifer.k@gmail.com>
2016-04-11 17:33:39 -0400

    Creating log in functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,7 +30,7 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-    redirect_to product_url
+    redirect_to products_url
   end
 
 private
